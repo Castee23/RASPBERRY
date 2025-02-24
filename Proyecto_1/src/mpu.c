d01d69d20a62fef364923e08104ab24ec3e942fd
Samsz0 <samuel.mszy@gmail.com>
2025-02-24 14:52:12 +0100

    Update mpu.c

--- Proyecto_1/src/mpu.c
+++ Proyecto_1/src/mpu.c
@@ -5,14 +5,14 @@ int mpu(void){
   int fd;
   uint8_t data[6];
   
-  i2c_read(fd, ACCEL_XOUT_H, data, 6);
-  i2c_read(fd, ACCEL_XOUT_L, data, 6);
+  i2c_read(fd, ACCEL_XOUT_H, data, 6, ADRESS);
+  i2c_read(fd, ACCEL_XOUT_L, data, 6, ADRESS);
   
-  i2c_read(fd, ACCEL_YOUT_H, data, 6);
-  i2c_read(fd, ACCEL_YOUT_L, data, 6);
+  i2c_read(fd, ACCEL_YOUT_H, data, 6, ADRESS);
+  i2c_read(fd, ACCEL_YOUT_L, data, 6, ADRESS);
 
-  i2c_read(fd, ACCEL_ZOUT_H, data, 6);
-  i2c_read(fd, ACCEL_ZOUT_L, data, 6);
+  i2c_read(fd, ACCEL_ZOUT_H, data, 6, ADRESS);
+  i2c_read(fd, ACCEL_ZOUT_L, data, 6, ADRESS);
 
   int16_t accel_x = (data[0] << 8) | data[1];
   int16_t accel_y = (data[2] << 8) | data[3];
