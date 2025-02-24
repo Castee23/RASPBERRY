be590b6d2c8d453a7bd2969abc8afe50fcde3be9
Samsz0 <samuel.mszy@gmail.com>
2025-02-24 13:51:29 +0100

    Update mpu.c

---
+++ Proyecto_1/src/mpu.c
@@ -0,0 +1,26 @@
+#include "Mpu6050.h"
+#include <stdio.h>
+
+int mpu(void){
+  int fd;
+  uint8_t data[6];
+  
+  i2c_read(fd, ACCEL_XOUT_H, data, 6);
+  i2c_read(fd, ACCEL_XOUT_L, data, 6);
+  
+  i2c_read(fd, ACCEL_YOUT_H, data, 6);
+  i2c_read(fd, ACCEL_YOUT_L, data, 6);
+
+  i2c_read(fd, ACCEL_ZOUT_H, data, 6);
+  i2c_read(fd, ACCEL_ZOUT_L, data, 6);
+
+  int16_t accel_x = (data[0] << 8) | data[1];
+  int16_t accel_y = (data[2] << 8) | data[3];
+  int16_t accel_z = (data[4] << 8) | data[5];
+
+  int x = accel_x / 16384.0;
+  int y = accel_y / 16384.0;
+  int z = accel_z / 16384.0;
+  
+  
+}
